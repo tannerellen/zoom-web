f0da3994344cacd44a17a206cc187e64b94cb368
Tanner Ellen <tanner@seedcode.com>
2025-08-26 15:51:13 -0700

    feat: changes zoom to open in chrome

--- app/zoom-web.sh
+++ app/zoom-web.sh
@@ -1,4 +1,22 @@
+#!/bin/bash
+
 appUrl="$@"
 destUrl=$(echo $appUrl | sed 's/zmw:\/\//https:\/\//')
-# hyprctl dispatch workspace emptynm & google-chrome-stable "$destUrl"
-firefox --new-window "$destUrl" && sleep 0.2 && hyprctl dispatch movetoworkspace emptynm
+
+launch_chrome() {
+    local url="$1"
+    
+    if pgrep -x "chrome" > /dev/null; then
+        echo "Chrome detected, opening new window"
+        google-chrome-stable --new-window "$url"
+    else
+        echo "Launching Chrome"
+        google-chrome-stable "$url"
+    fi
+}
+
+hyprctl dispatch workspace empty &
+launch_chrome "$destUrl"
+
+# Old firefox method. No longer used due to firefox low quality on zoom
+# firefox --new-window "$destUrl" && sleep 0.2 && hyprctl dispatch movetoworkspace emptynm
